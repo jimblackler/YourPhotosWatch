a682a8db7ba37d23658b474e0cb66844f0b81c99
Jim Blackler <jimblackler@google.com>
2015-01-05 21:54:18 +0000

    Added link to Android Wear for watch select.

--- Application/src/main/java/net/jimblackler/yourphotoswatch/MainActivity.java
+++ Application/src/main/java/net/jimblackler/yourphotoswatch/MainActivity.java
@@ -3,15 +3,20 @@ package net.jimblackler.yourphotoswatch;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends Activity {
 
+  private static final String TAG = MainActivity.class.getName();
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
+
     setContentView(R.layout.main_activity);
     findViewById(R.id.select_from_phone).setOnClickListener(new View.OnClickListener() {
       @Override
@@ -33,6 +38,17 @@ public class MainActivity extends Activity {
         startActivity(new Intent(MainActivity.this, FacebookSelectActivity.class));
       }
     });
+
+
+    if (!getIntent().getAction().endsWith(".CONFIG")) {
+      findViewById(R.id.launch_config).setVisibility(View.VISIBLE);
+      findViewById(R.id.launch_config).setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+          startActivity(getPackageManager().getLaunchIntentForPackage("com.google.android.wearable.app"));
+        }
+      });
+    }
 
     findViewById(R.id.about).setOnClickListener(new View.OnClickListener() {
       @Override
